05b29952b343666087c44b9240610cf73c213fe5
Owais Ahmed <ra920453@gmail.com>
2026-09-25 17:17:22 +0500

    fix: replace deprecated react-native-cookies with preeternal fork to fix gradle build

--- frontend/src/components/WebViewLogin.tsx
+++ frontend/src/components/WebViewLogin.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, ActivityIndicator } from 'react-native';
 import { WebView } from 'react-native-webview';
-import CookieManager from '@react-native-cookies/cookies';
+import CookieManager from '@preeternal/react-native-cookie-manager';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface WebViewLoginProps {
